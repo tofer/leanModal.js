fe20bb091a66c59d43e32d46e72f7aff6e4ca3ba
Chris Curtis <tofer.curtis@gmail.com>
2014-01-08 16:59:27 -0800

    Update jquery.leanModal.js
    Added "auto" as supported value for "top" to center modal vertically on page

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,5 +1,5 @@
 (function($){
- 
+
     $.fn.extend({ 
          
         leanModal: function(options) {
@@ -23,53 +23,56 @@
                
                 $(this).click(function(e) {
               
-              	var modal_id = $(this).attr("href");
-
-		if (o.clickOutToClose) {
-			$("#lean_overlay").click(function() { 
-	                     close_modal(modal_id);                    
-	                });
-		}
-                
-                $(o.closeButton).click(function() { 
-                     close_modal(modal_id);                    
-                });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
-
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
-
-        		$('#lean_overlay').fadeTo(200,o.overlay);
-
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
-
-        		$(modal_id).fadeTo(200,1);
-
-                e.preventDefault();
-                		
+                  	var modal_id = $(this).attr("href");
+    
+            		if (o.clickOutToClose) {
+            			$("#lean_overlay").click(function() { 
+    	                     close_modal(modal_id);                    
+    	                });
+            		}
+                    
+                    $(o.closeButton).click(function() { 
+                         close_modal(modal_id);                    
+                    });
+                             	
+                  	var modal_height = $(modal_id).outerHeight();
+            	  	var modal_width = $(modal_id).outerWidth();
+            
+            		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+            
+            		$('#lean_overlay').fadeTo(200,o.overlay);
+    
+            		$(modal_id).css({ 
+            			'display' : 'block',
+            			'position' : 'fixed',
+            			'opacity' : 0,
+            			'z-index': 11000,
+            			'left' : 50 + '%',
+            			'margin-left' : -(modal_width/2) + "px",
+            			'top' : o.top + "px"
+            		});
+            		
+            		if ('auto' == o.top) {
+            			$(modal_id).css({
+            				'top' : 50 + '%',
+            				'margin-top' : -(modal_height/2) + "px"
+            			});
+            		} else {
+            			$(modal_id).css({
+            				'top' : o.top + "px"
+            			});
+            		}
+            
+            		$(modal_id).fadeTo(200,1);
+    
+                    e.preventDefault();
               	});
-             
             });
 
-			function close_modal(modal_id){
-
+    		function close_modal(modal_id){
         		$("#lean_overlay").fadeOut(200);
-
         		$(modal_id).css({ 'display' : 'none' });
-			
-			}
-    
+    		}
         }
     });
      
